116f1a4d10661a317aedc1ccad925f3faf9aaf5b
FabioPenedo <fabiopenedo21@gmail.com>
2024-04-25 15:51:22 -0300

    primeiro commit, exercicio-metodo-abstrato

---
+++ App/Entities/Enums/Color.cs
@@ -0,0 +1,9 @@
+﻿namespace App.Entities.Enums
+{
+    enum Color
+    {
+        Black,
+        Blue,
+        Red
+    }
+}
---
+++ App/Entities/Shape.cs
@@ -0,0 +1,15 @@
+﻿using App.Entities.Enums;
+
+namespace App.Entities
+{
+    abstract internal class Shape
+    {
+        public Color Color { get; set; }
+
+        public Shape(Color color)
+        {
+            Color = color;
+        }
+        public abstract double Area();
+    }
+}
